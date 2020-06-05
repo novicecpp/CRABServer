44735078825aeeedca0e2d6bff2d399a04e948da
dciangot <dciangot@cern.ch>
2020-06-05 10:18:57 +0200

    source Rucio environment from cvmfs

--- scripts/task_process/task_proc_wrapper.sh
+++ scripts/task_process/task_proc_wrapper.sh
@@ -16,7 +16,7 @@ function manage_transfers {
         DEST_LFN=`python -c 'import sys, json; print json.loads( open("task_process/transfers.txt").readlines()[0] )["destination_lfn"]' `
 
         if [[ $DEST_LFN =~ ^/store/test/rucio/* ]]; then
-        export PYTHONPATH=$PYTHONPATH:/cvmfs/cms.cern.ch/rucio/current/lib/python2.7/site-packages
+        source /cvmfs/cms.cern.ch/rucio/setup.sh
         timeout 15m python task_process/RUCIO_Transfers.py
         else
         timeout 15m python task_process/FTS_Transfers.py
