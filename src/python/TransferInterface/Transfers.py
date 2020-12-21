10e541a5b3883336f93b10e3a418e96030ef49f8
dciangot <dciangot@cern.ch>
2020-12-21 08:53:25 +0000

    first split of files

---
+++ src/python/TransferInterface/Transfers.py
@@ -0,0 +1,18 @@
+"""
+Transfers parent class for both FTS and RUCIO stageout task processes
+"""
+
+
+class Transfers(object):
+    def __init__(self, config, logWarning, logDebug):
+        """
+        """
+    def register():
+        """
+        """
+    def monitor():
+        """
+        """
+    def update():
+        """
+        """
