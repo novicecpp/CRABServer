17b0f81e160599b3fe01d9ab4ddba3fe43bd5bc9
Thanayut Seethongchuen <=>
2022-05-23 19:46:14 +0200

    add api compare with different crabserver instance

---
+++ test/checkingRESTResponse/env.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/home/thanayut/myhome/coding/cern/fork/CRABServer/src/python/:/home/thanayut/myhome/coding/cern/fork/WMCore/src/python/:$PYTHONPATH
+#export X509_USER_PROXY=/tmp/x509up_u145867
+#export X509_CERT_DIR=/etc//certificates
