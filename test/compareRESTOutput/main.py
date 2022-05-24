0dfc555376bd9349be35e2203e7215f1e2efd1ac
Thanayut Seethongchuen <=>
2022-05-24 19:14:38 +0200

    various
    - use os.getenv to replace default value
    - change data format. only request string need from url because it
      only support GET request (for now)
    - fix variable when do looping
    - move read requests's file inside create_requests function (and
      rename the function

--- test/compareRESTOutput/main.py
+++ test/compareRESTOutput/main.py
@@ -7,9 +7,7 @@ import pprint
 import argparse
 from RESTInteractions import CRABRest
 from urllib.parse import urlparse, parse_qs
-from utils import EnvDefault
 
-X509_USER_PROXY = os.environ['X509_USER_PROXY']
 
 logger = logging.getLogger('CRABTest')
 handler = logging.StreamHandler()
@@ -17,23 +15,29 @@ formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(module)s:%(message)s'
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def create_requests(list_request_str):
+def create_requests_from_file(path):
+    """read list of request from file and convert it to object
+    content of file are consist of GET request string seperate by newline
+    e.g /crabserver/prod/info?subresource=version
+    comment with # is supported
+
+    return list of dict
+    """
+    with open(path, 'r') as r:
+        list_request_raw = r.readlines()
     ret = []
-    for s in list_request_str.split('\n'):
-        if s.strip() == '':
+    for s in list_request_raw:
+        if s.strip() == '' or s.strip().startswith('#'):
             continue
-        elif s.strip().startswith('#'):
-            continue
-        method, request = s.split(' ')
+        request = s.strip()
         url = urlparse(s)
         api = url.path.split('/')[-1]
         data = parse_qs(url.query)
-        ret.append({'method': method,
-                    'request': request,
-                    'request_hash': hashlib.md5(request.encode()).hexdigest(),
-                    'api': api,
-                    'data': data,
-                    })
+        request = {'request': request,
+                   'request_hash': hashlib.md5(request.encode()).hexdigest(),
+                   'api': api,
+                   'data': data}
+        ret.append(request)
     return ret
 
 def create(requests, output_dir, restclient):
@@ -41,19 +45,21 @@ def create(requests, output_dir, restclient):
     """
     for request in requests:
         output_path = os.path.join(output_dir, request['request_hash']) + '.json'
-        logger.debug(f"Retrieving {request['method']} /{request['api']}")
+        logger.debug(f"Retrieving GET /{request['api']}")
         output = restclient.get(request['api'], request['data'])[0]
         with open(output_path, 'w') as w:
             json.dump(output, w)
 
 def compare(requests, input_dir, rest_client):
+    """compare output from request list with json
+    """
     for request in requests:
-        logger.debug(f"Retrieving {request['method']} /{request['api']}")
+        logger.debug(f"Retrieving GET /{request['api']}")
         server_output = crabserver.get(request['api'], request['data'])[0]
         with open(os.path.join(input_dir, request['request_hash'] + '.json'), 'r') as r:
             expected_output = json.load(r)
         compare_result = server_output == expected_output
-        logger.info(f"{request['method']} /{request['api']} compare result: {compare_result}")
+        logger.info(f"GET /{request['api']} compare result: {compare_result}")
         if compare_result:
             logger.debug("\nServer output:\n%s\n\nexpected output:\n%s", pprint.pformat(server_output), pprint.pformat(expected_output))
         else:
@@ -67,34 +73,30 @@ if __name__ == '__main__':
     parser.add_argument('mode', type=str, help='mode')
     parser.add_argument('url_path', type=str, help='url file path')
     parser.add_argument('--db',
-                        action=EnvDefault,
-                        envvar='DATABASE_INSTANCE',
                         type=str,
+                        default=os.getenv('DATABASE_INSTANCE', 'devtwo'),
                         help='database instance (dev/devtwo/preprod/prod')
     parser.add_argument('--rest',
                         type=str,
-                        action=EnvDefault,
-                        envvar='CRABSERVER_HOSTNAME',
-                        default='cmsweb-test11.cern.ch',
+                        default=os.getenv('CRABSERVER_HOSTNAME','cmsweb-test11.cern.ch'),
                         help='rest hostname e.g. cmsweb-test11.cern.ch.')
     parser.add_argument('--userproxy',
                         type=str,
-                        action=EnvDefault,
-                        envvar='X509_USER_PROXY',
-                        default='devtwo',
+                        default=os.getenv('X509_USER_PROXY', '/tmp/cert.pem'),
                         help='user proxy file path')
     parser.add_argument('--loglevel',
                         type=str,
                         default='INFO',
-                        help='log level')
+                        help='log level (uppercase)')
     args = parser.parse_args()
-    logger.setLevel(args.loglevel)
 
-    with open(args.url_path, 'r') as r:
-        requests = create_requests(r.read())
+    logger.setLevel(args.loglevel)
+    logger.info("%s", args)
+
+    requests = create_requests_from_file(args.url_path)
     crabserver = CRABRest(hostname=args.rest,
-                          localcert=X509_USER_PROXY,
-                          localkey=X509_USER_PROXY,
+                          localcert=args.userproxy,
+                          localkey=args.userproxy,
                           userAgent='CRABRest',
                           logger=logger,
                           version='0.0.0')
