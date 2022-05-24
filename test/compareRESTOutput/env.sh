61dd9bcc17c118af68e7f66a20b6fb9cf2a225b9
Thanayut Seethongchuen <=>
2022-05-24 14:19:08 +0200

    remove unused file

--- test/compareRESTOutput/env.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=/home/thanayut/myhome/coding/cern/fork/CRABServer/src/python/:/home/thanayut/myhome/coding/cern/fork/WMCore/src/python/:$PYTHONPATH
-#export X509_USER_PROXY=/tmp/x509up_u145867
-#export X509_CERT_DIR=/etc//certificates
