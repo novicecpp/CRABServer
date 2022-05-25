bb3b72342e44e89b0e9ed9216e22aef3e25d8524
Thanayut Seethongchuen <=>
2022-05-25 20:54:47 +0200

    add testapi and print request_trace_id

---
+++ src/python/CRABInterface/RESTTestAPI.py
@@ -0,0 +1,36 @@
+import logging
+
+# WMCore dependecies here
+from WMCore.REST.Server import RESTEntity, restcall
+from WMCore.REST.Validation import validate_str
+from cherrypy import request
+
+# CRABServer dependecies here
+from CRABInterface.RESTExtensions import authz_login_valid
+from CRABInterface.Regexps import RX_SUBRES_SI, RX_TASKNAME
+import time
+import random
+
+
+class RESTTestAPI(RESTEntity):
+    """REST entity for workflows and relative subresources"""
+
+    def __init__(self, app, api, config, mount):
+        RESTEntity.__init__(self, app, api, config, mount)
+        self.logger = logging.getLogger("CRABLogger:RESTTestAPI")
+
+    def validate(self, apiobj, method, api, param, safe):
+        """Validating all the input parameter as enforced by the WMCore.REST module"""
+        authz_login_valid()
+        #if method in ['GET']:
+        #    validate_str('subresource', param, safe, RX_SUBRES_SI, optional=True)
+        #    validate_str('workflow', param, safe, RX_TASKNAME, optional=True)
+
+    @restcall
+    def get(self, **kwargs):
+        """Retrieves the server information, like delegateDN, filecacheurls ...
+           :arg str subresource: the specific server information to be accessed;
+        """
+        self.logger.info('testlog trace=%s', request.request_trace_id)
+        ret = [{"crabserver":"Welcome to himalaya"}]
+        return ret
