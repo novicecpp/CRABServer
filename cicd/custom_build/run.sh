7904b863536b258b7cdfd1219529ff3695c573d9
Thanayut Seethongchuen <=>
2022-07-06 11:59:42 +0200

    tag image

--- cicd/custom_build/run.sh
+++ cicd/custom_build/run.sh
@@ -9,4 +9,6 @@ git clone https://github.com/novicecpp/CRABServer -b custom_build crabserver_ci
 cp -r crabserver_ci/cicd/custom_build/* .
 git clone $CRABSERVER_REPO -b $CRABSERVER_BRANCH --depth 1
 git clone $WMCORE_REPO -b $WMCORE_BRANCH --depth 1
-docker build -f Dockerfile .
+CRABSERVER_HASH=$(cd CRABServer && git rev-parse HEAD | head -c8)
+WMCORE_HASH=$(cd WMCore && git rev-parse HEAD | head -c8)
+docker build -f Dockerfile -t registry.cern.ch/cmscrab/crabserver:crabserver_$CRABSERVER_HASH-wmcore_$WMCORE_HASH .
