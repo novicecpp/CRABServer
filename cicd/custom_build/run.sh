76c82991e7b50535d8e4a9f40f56cab4e018e01b
Thanayut Seethongchuen <=>
2022-07-21 11:43:28 +0200

    change image tag and create parameters file for deploy jenkins job

--- cicd/custom_build/run.sh
+++ cicd/custom_build/run.sh
@@ -9,7 +9,7 @@ git clone $CRABSERVER_REPO -b $CRABSERVER_BRANCH --depth 1
 git clone $WMCORE_REPO -b $WMCORE_BRANCH --depth 1
 CRABSERVER_HASH=$(cd CRABServer && git rev-parse HEAD | head -c8)
 WMCORE_HASH=$(cd WMCore && git rev-parse HEAD | head -c8)
-DOCKER_IMAGE_NAMETAG=registry.cern.ch/cmscrab/crabserver:crabserver_$CRABSERVER_HASH-wmcore_$WMCORE_HASH
+DOCKER_IMAGE_NAMETAG=registry.cern.ch/cmscrab/crabserver:crabserver_$CRABSERVER_HASH.wmcore_$WMCORE_HASH
 echo "(DEBUG) new image: $DOCKER_IMAGE_NAMETAG"
 docker build \
        -f Dockerfile \
@@ -22,3 +22,7 @@ docker build \
 export DOCKER_CONFIG=$PWD/docker_login
 docker login registry.cern.ch --username $HARBOR_CMSCRAB_USERNAME --password-stdin <<< $HARBOR_CMSCRAB_PASSWORD
 docker push $DOCKER_IMAGE_NAMETAG
+
+
+echo "DOCKER_IMAGE_NAMETAG=registry.cern.ch/cmscrab/crabserver:crabserver_$CRABSERVER_HASH.wmcore_$WMCORE_HASH" >> jenkins_parameters
+echo "REST_Instance=${REST_Instance:-test11}">> jenkins_parameters
