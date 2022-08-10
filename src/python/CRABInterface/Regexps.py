5ddbac6fe4217a704d6e9ba44203f0aec74b1fcb
Thanayut Seethongchuen <=>
2022-08-10 15:42:07 +0200

    add subresource of testapi

--- src/python/CRABInterface/Regexps.py
+++ src/python/CRABInterface/Regexps.py
@@ -138,3 +138,6 @@ RX_ASO_WORKERNAME = RX_WORKER_NAME
 
 RX_SUBGETUSERTRANSFER = re.compile(r"^(getById|getTransferStatus|getPublicationStatus)$")
 RX_SUBPOSTUSERTRANSFER = re.compile(r"^(killTransfers|retryPublication|retryTransfers|killTransfersById|updateDoc)$")
+
+#subresources of testapi
+RX_SUBRES_CACHE = re.compile(r"^(error)$")
