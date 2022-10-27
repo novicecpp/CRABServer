94b6c0f0e95d99974b64d791476088fed39e132f
Thanayut Seethongchuen <=>
2022-10-27 18:20:30 +0200

    Avoid MasterWorker stuck because recurring modules not found

--- src/python/TaskWorker/MasterWorker.py
+++ src/python/TaskWorker/MasterWorker.py
@@ -9,6 +9,7 @@ import sys
 import time
 import signal
 import logging
+import traceback
 
 from http.client import HTTPException
 from MultiProcessingLog import MultiProcessingLog
@@ -240,11 +241,16 @@ class MasterWorker(object):
             self.slaves = Worker(self.config, self.restHost, self.dbInstance)
         self.slaves.begin()
         recurringActionsNames = getattr(self.config.TaskWorker, 'recurringActions', [])
-        self.recurringActions = [self.getRecurringActionInst(name) for name in recurringActionsNames]
+        self.recurringActions = [m for m in [self.getRecurringActionInst(name) for name in recurringActionsNames] if m is not None]
 
 
     def getRecurringActionInst(self, actionName):
-        mod = __import__('TaskWorker.Actions.Recurring.%s' % actionName, fromlist=actionName)
+        try:
+            mod = __import__('TaskWorker.Actions.Recurring.%s' % actionName, fromlist=actionName)
+        except ModuleNotFoundError:
+            traceback.print_exec()
+            self.logger.error('Recurring Action [%s] not found, skipping')
+            return
         return getattr(mod, actionName)(self.config.TaskWorker.logsDir)
 
 
@@ -355,8 +361,8 @@ class MasterWorker(object):
                 failstatus = 'FAILED'
             self.updateWork(taskname, command, failstatus)
             warning = 'username %s banned in CRAB TaskWorker configuration' % task['tm_username']
-            configreq = {'subresource': 'addwarning', 
-                         'workflow': taskname, 
+            configreq = {'subresource': 'addwarning',
+                         'workflow': taskname,
                          'warning': warning}
             try:
                 self.crabserver.post(api='task', data=urlencode(configreq))
