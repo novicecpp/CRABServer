aac6e83c19383d6b7058842d3ae44fc52b7c5619
Thanayut Seethongchuen <=>
2022-11-09 13:44:29 +0100

    remove pdb

--- src/python/CRABInterface/RESTUserWorkflow.py
+++ src/python/CRABInterface/RESTUserWorkflow.py
@@ -466,7 +466,6 @@ class RESTUserWorkflow(RESTEntity):
             validate_num("partialdataset", param, safe, optional=True)
             validate_num("requireaccelerator", param, safe, optional=True)
             if safe.kwargs.get("requireaccelerator", None):
-                import pdb; pdb.set_trace()
                 validate_str("acceleratorparams", param, safe, RX_ANYTHING, optional=True)
                 safe.kwargs["acceleratorparams"] = self._parseAcceleratorParams(safe.kwargs["acceleratorparams"])
             else:
