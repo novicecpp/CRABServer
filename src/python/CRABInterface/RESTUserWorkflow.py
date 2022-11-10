809cac280356262b428dcf49f4fed304ad78edb2
Thanayut Seethongchuen <=>
2022-11-10 16:00:30 +0100

    yet another pdb

--- src/python/CRABInterface/RESTUserWorkflow.py
+++ src/python/CRABInterface/RESTUserWorkflow.py
@@ -426,6 +426,7 @@ class RESTUserWorkflow(RESTEntity):
                     validate_num("GPUMemoryMB", accParams, accSafe, optional=True, minval=0)
                     validate_strlist("CUDACapabilities", accParams, accSafe, RX_CUDA_VERSION)
                     validate_str("CUDARuntime", accParams, accSafe, RX_CUDA_VERSION, optional=True)
+            import pdb; pdb.set_trace()
 
 
         elif method in ['POST']:
