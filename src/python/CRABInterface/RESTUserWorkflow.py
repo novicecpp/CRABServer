134d2f6237b75ed6b33d3352de6e528873ab7eeb
Thanayut Seethongchuen <=>
2022-11-23 23:43:02 +0100

    validate_str and check

--- src/python/CRABInterface/RESTUserWorkflow.py
+++ src/python/CRABInterface/RESTUserWorkflow.py
@@ -418,15 +418,14 @@ class RESTUserWorkflow(RESTEntity):
             validate_num("partialdataset", param, safe, optional=True)
             validate_num("requireaccelerator", param, safe, optional=True)
             # validate optional acceleratorparams
-            if param.kwargs.get("acceleratorparams", None):
+            validate_str("acceleartorparams", param, safe, RX_ANYTHING, optional=True)
+            if safe.kwargs["acceleratorparams"]:
                 if not safe.kwargs["requireaccelerator"] and safe.kwargs["acceleratorparams"]:
                     raise InvalidParameter("There are accelerator parameters but requireAccelerator is False")
                 with validate_dict("acceleratorparams", param, safe, optional=True) as (accParams, accSafe):
                     validate_num("GPUMemoryMB", accParams, accSafe, minval=0)
                     validate_strlist("CUDACapabilities", accParams, accSafe, RX_CUDA_VERSION)
                     validate_str("CUDARuntime", accParams, accSafe, RX_CUDA_VERSION, optional=True)
-            else:
-                safe.kwargs["acceleratorparams"] = None
 
 
         elif method in ['POST']:
