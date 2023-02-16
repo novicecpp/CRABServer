c7fc0a2b8c23ca7ab850b0947e80acc2a97dc6af
Thanayut Seethongchuen <=>
2023-02-16 18:29:32 +0100

    remove debugging exception

--- src/python/TaskWorker/Actions/DBSDataDiscovery.py
+++ src/python/TaskWorker/Actions/DBSDataDiscovery.py
@@ -101,11 +101,6 @@ class DBSDataDiscovery(DataDiscovery):
             The exception message contains details and an attempt is done to upload it to TaskDB
             so that crab status can report it
         """
-        # debugging
-        self.logger.debug(blockList)
-        self.logger.debug(msgHead)
-        self.uploadWarning(msgHead, self.userproxy, self.taskName)
-        raise Exception("'DBSDataDiscovery.requestTapeRecall()' you shall not pass")
         msg = msgHead
         if system == 'Rucio':
             # need to use crab_tape_recall Rucio account to create containers and create rules
@@ -505,8 +500,6 @@ class DBSDataDiscovery(DataDiscovery):
                 msg += '\nIf you need the full dataset, contact Data Transfer team via %s' % FEEDBACKMAIL
                 self.logger.warning(msg)
                 self.uploadWarning(msg, self.userproxy, self.taskName)
-
-        raise Exception("'DBSDataDiscovery.executeInternal()' you shall not pass")
 
         # will not need lumi info if user has asked for split by file with no run/lumi mask
         splitAlgo = kwargs['task']['tm_split_algo']
