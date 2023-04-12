b3eeadf65a4d61fa0b313e4982c0a5c3961be295
Thanayut Seethongchuen <=>
2023-04-12 20:38:18 +0200

    RegisterReplicas.prepare

---
+++ src/python/ASO/Rucio/Actions/RegisterReplicas.py
@@ -0,0 +1,55 @@
+import logging
+from rucio.rse.rsemanager import find_matching_scheme
+
+from ASO.Rucio.exception import RucioTransferException
+
+
+class RegisterReplicas:
+    def __init__(self, transfer, rucioClient):
+        self.logger = logging.getLogger("RucioTransfer.Actions.RegisterReplicas")
+        self.rucioClient = rucioClient
+        self.transfer = transfer
+    def execute(self):
+        raise NotImplementedError
+    def register(self):
+        raise NotImplementedError
+    def prepare(self, transferList):
+        # create bucket rse
+        bucket = {}
+        replicasByRSE = {}
+        for xdict in transferList:
+            rse = xdict["source"]
+            if not rse in bucket:
+                bucket[rse] = []
+            bucket[rse].append(xdict)
+        for rse in bucket:
+            xdict = bucket[rse][0]
+            pfn = self.getSourcePFN(xdict["source_lfn"], rse, xdict["destination"])
+            pfnPrefix = pfn.split(xdict["source_lfn"])[0]
+            replicasByRSE[rse] = []
+            for xdict in bucket[rse]:
+                replica = {
+                    'scope': self.transfer.rucioScope,
+                    'pfn': f'{pfnPrefix}{xdict["source_lfn"]}',
+                    'name': xdict['destination_lfn'],
+                    'bytes': xdict['filesize'],
+                    'adler32': xdict['checksums']['adler32'],
+                }
+                replicasByRSE[rse].append(replica)
+        return replicasByRSE
+
+
+
+    def getSourcePFN(self, sourceLFN, sourceRSE, destinationRSE):
+        try:
+            _, srcScheme, _, _ = find_matching_scheme(
+                {"protocols": self.rucioClient.get_protocols(destinationRSE)},
+                {"protocols": self.rucioClient.get_protocols(sourceRSE)},
+                "third_party_copy_read",
+                "third_party_copy_write",
+            )
+            did = f'{self.transfer.rucioScope}:{sourceLFN}'
+            sourcePFNMap = self.rucioClient.lfns2pfns(sourceRSE, [did], operation="third_party_copy_read", scheme=srcScheme)
+            return sourcePFNMap[did]
+        except Exception as ex:
+            raise RucioTransferException("Failed to get source PFN") from ex
---
+++ test/python/ASO/Rucio/test_RegisterReplicas.py
@@ -0,0 +1,103 @@
+import pytest
+import json
+from unittest.mock import patch, Mock
+
+from ASO.Rucio.Actions.RegisterReplicas import RegisterReplicas
+
+# prepare_replicas
+# input is just transfers.txt start from last lines
+# {"id": "7e6d075f7434f1307a764d491d86ab1192554b76106d139846810834", "username": "tseethon", "taskname": "230227_174038:tseethon_crab_rucio_transfer_test12_20230227_184034", "start_time": 1677520683, "destination": "T2_CH_CERN", "destination_lfn": "/store/user/rucio/tseethon/test-workflow/GenericTTbar/autotest-1677519634/230227_174038/0000/output_2.root", "source": "T3_US_FNALLPC", "source_lfn": "/store/temp/user/tseethon.d6830fc3715ee01030105e83b81ff3068df7c8e0/tseethon/test-workflow/GenericTTbar/autotest-1677519634/230227_174038/0000/output_2.root", "filesize": 630710, "publish": 0, "transfer_state": "NEW", "publication_state": "NOT_REQUIRED", "job_id": "2", "job_retry_count": 1, "type": "output", "publishname": "autotest-1677519634-00000000000000000000000000000000", "checksums": {"adler32": "5cbf440e", "cksum": "3473488862"}, "outputdataset": "/GenericTTbar/tseethon-autotest-1677519634-94ba0e06145abd65ccb1d21786dc7e1d/USER"}
+# expect output is MAP OF RSE key replicas in RSE
+# {rse1: [replica1,replicas2], rse2: [replica3, replica4]}
+# where
+# replica = {'scope': glob.rucio_scope, 'pfn': pfn_map[rse][source_lfn],
+#            'name': destination_lfn, 'bytes': size, 'adler32': checksum}
+# and
+# pfn_map is map of ALL lfn of input in format of
+# pfn_map = {rse1: {source_lfn1: pfn}}
+# well let change to make it more simpler
+
+@pytest.fixture
+def mock_rucioClient():
+    with patch('rucio.client.client.Client', autospec=True) as m_rucioClient:
+        return m_rucioClient
+
+@pytest.fixture
+def mock_Transfer():
+    username = 'cmscrab'
+    rucioScope = f'user.{username}'
+    publishname = '/TestPrimary/test-dataset/RAW'
+    currentDatasetUUID = 'c9b28b96-5d16-41cd-89af-2678971132c9'
+    currentDataset = f'{publishname}#{currentDatasetUUID}'
+    logsDataset = f'{publishname}#LOG'
+    return Mock(publishname=publishname, currentDataset=currentDataset, rucioScope=rucioScope, logsDataset=logsDataset, currentDatasetUUID=currentDatasetUUID, username=username)
+
+@pytest.fixture
+def loadTransferList():
+    with open('test/assets/transfers.txt', encoding='utf-8') as r:
+        return [json.loads(line) for line in r]
+
+
+def test_getSourcePFN(mock_Transfer, mock_rucioClient):
+    srcRSE = 'T3_US_FNALLPC'
+    dstRSE = 'T2_CH_CERN'
+    srcLFN = '/store/temp/user/tseethon.d6830fc3715ee01030105e83b81ff3068df7c8e0/tseethon/test-workflow/GenericTTbar/autotest-1679671056/230324_151740/0000/output_17.root'
+    srcPFN = 'root://eoscms.cern.ch//eos/cms/store/temp/user/tseethon.d6830fc3715ee01030105e83b81ff3068df7c8e0/tseethon/test-workflow/GenericTTbar/autotest-1679671056/230324_151740/0000/output_17.root'
+    srcDid = f'{mock_Transfer.rucioScope}:{srcLFN}'
+    returnLfns2pfns = {srcDid: srcPFN}
+    mock_rucioClient.lfns2pfns.return_value = returnLfns2pfns
+    with patch('ASO.Rucio.Actions.RegisterReplicas.find_matching_scheme', autospec=True) as mock_find_matching_scheme:
+        mock_find_matching_scheme.return_value = ('', 'davs', '', '')
+        r = RegisterReplicas(mock_Transfer, mock_rucioClient)
+        assert r.getSourcePFN(srcLFN, srcRSE, dstRSE) == srcPFN
+        mock_find_matching_scheme.assert_called_once()
+    mock_rucioClient.get_protocols.assert_called()
+    mock_rucioClient.lfns2pfns.assert_called_once()
+
+def test_prepare(mock_Transfer, mock_rucioClient):
+    # input
+    prepareInput = [{
+        "id": "98f353b91ec84f0217da80bde84d6b520c0c6640f60ad9aabb7b20ca",
+        "username": "cmscrab",
+        "taskname": "230324_151740:tseethon_crab_rucio_transfer_whitelist_cern_test12_20230324_161736",
+        "start_time": 1679671544,
+        "destination": "T2_CH_CERN",
+        "destination_lfn": "/store/user/rucio/tseethon/test-workflow/GenericTTbar/autotest-1679671056/230324_151740/0000/output_9.root",
+        "source": "T2_CH_CERN",
+        "source_lfn": "/store/temp/user/tseethon.d6830fc3715ee01030105e83b81ff3068df7c8e0/tseethon/test-workflow/GenericTTbar/autotest-1679671056/230324_151740/0000/output_9.root",
+        "filesize": 628054,
+        "publish": 0,
+        "transfer_state": "NEW",
+        "publication_state": "NOT_REQUIRED",
+        "job_id": "9",
+        "job_retry_count": 0,
+        "type": "output",
+        "publishname": "autotest-1679671056-00000000000000000000000000000000",
+        "checksums": {
+            "adler32": "812b8235",
+            "cksum": "1236675270"
+        },
+        "outputdataset": "/GenericTTbar/tseethon-autotest-1679671056-94ba0e06145abd65ccb1d21786dc7e1d/USER"
+    }]
+
+    # expectedOuput should be
+    getSourcePFNReturnValue = 'davs://eoscms.cern.ch:443/eos/cms/store/temp/user/tseethon.d6830fc3715ee01030105e83b81ff3068df7c8e0/tseethon/test-workflow/GenericTTbar/autotest-1679671056/230324_151740/0000/output_9.root'
+    expectedOutput = {
+        "T2_CH_CERN": [
+            {
+                "scope": "user.cmscrab",
+                "pfn": getSourcePFNReturnValue,
+                "name": "/store/user/rucio/tseethon/test-workflow/GenericTTbar/autotest-1679671056/230324_151740/0000/output_9.root",
+                "bytes": 628054,
+                "adler32": "812b8235",
+            }
+        ]
+    }
+    with patch('ASO.Rucio.Actions.RegisterReplicas.RegisterReplicas.getSourcePFN', autospec=True) as mock_getSourcePFN:
+        mock_getSourcePFN.return_value = getSourcePFNReturnValue
+        r = RegisterReplicas(mock_Transfer, mock_rucioClient)
+        assert r.prepare(prepareInput) == expectedOutput
+
+@pytest.mark.skip(reason="Need to implement (or not?) but skip it for now.")
+def test_prepare_skip_direct_stageout(mock_Transfer, mock_rucioClient, loadTransferList):
+    pass
