ef7fffd226a74c2f3f26b7475bbce8bb62e729d2
Thanayut Seethongchuen <=>
2023-05-02 06:48:59 +0200

    remove RucioTransferMain to make code more simpler

--- src/python/ASO/Rucio/Main.py
+++ src/python/ASO/Rucio/Main.py
@@ -1,49 +1,17 @@
 import logging
-
 from argparse import ArgumentParser
 
 import ASO.Rucio.config as config
 from ASO.Rucio.RunTransfer import RunTransfer
 from ASO.Rucio.exception import RucioTransferException
 
-
-class RucioTransferMain:
-    """
-    This class is entrypoint of RUCIO_Transfers.py It setup logs
-    handlers and called RunTransfer to execute RUCIO_Transfers.py
-    algorithm.
-    """
-    def __init__(self):
-        self._initLogger()
-        self.logger = logging.getLogger('RucioTransfer.RucioTransferMain')
-
-    def run(self):
-        """
-        Execute RunTransfer.algorithm.
-        Exception handling here is for debugging purpose. If
-        RucioTransferException was raise, it mean some condition is
-        not meet and we want to fail (fast) this process.  But if
-        Exception is raise mean something gone wrong with our code and
-        need to investigate.
-        """
-        self.logger.info("executing RunTransfer")
-        try:
-            self.logger.info('executing RunTransfer')
-            run = RunTransfer()
-            run.algorithm()
-        except RucioTransferException as ex:
-            raise ex
-        except Exception as ex:
-            raise Exception("Unexpected error during main") from ex
-        self.logger.info('transfer completed')
-
-    def _initLogger(self):
-        logger = logging.getLogger('RucioTransfer')
-        logger.setLevel(logging.DEBUG)
-        hldr = logging.StreamHandler()
-        formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
-        hldr.setFormatter(formatter)
-        logger.addHandler(hldr)
+def initLogger():
+    logger = logging.getLogger('RucioTransfer')
+    logger.setLevel(logging.DEBUG)
+    hldr = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+    hldr.setFormatter(formatter)
+    logger.addHandler(hldr)
 
 
 def main():
@@ -85,10 +53,27 @@ def main():
     # class before execute test.
     config.args = opts
 
-    rucioTransfer = RucioTransferMain()
+    # init logger
+    initLogger()
+    logger = logging.getLogger('RucioTransfer')
 
-    rucioTransfer.run()
 
+    # Execute RunTransfer.algorithm().
+    # Exception handling here is for debugging purpose. If
+    # RucioTransferException was raise, it mean some condition is
+    # not meet and we want to fail (fast) this process.  But if
+    # Exception is raise mean something gone wrong with our code and
+    # need to investigate.
+    logger.info("executing RunTransfer")
+    try:
+        logger.info('executing RunTransfer')
+        run = RunTransfer()
+        run.algorithm()
+    except RucioTransferException as ex:
+        raise ex
+    except Exception as ex:
+        raise Exception("Unexpected error during main") from ex
+    logger.info('transfer completed')
 
 if __name__ == "__main__":
     main()
