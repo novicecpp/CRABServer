86b4b39873ecced515610b6f9d1148fde4cb39da
Thanayut Seethongchuen <=>
2023-11-30 20:25:19 +0100

    hardcode path

--- cicd/gitlab/execute_status_tracking.sh
+++ cicd/gitlab/execute_status_tracking.sh
@@ -2,4 +2,5 @@
 
 cd "$(git rev-parse --show-toplevel)"
 source test/container/testingScripts/setupCRABClient.sh;
-./test/container/testingScripts/statusTracking.py
+export PYTHONPATH=/cvmfs/cms.cern.ch/share/cms/crab-prod/v3.231010.00/lib:$PYTHONPATH
+python3 ./test/container/testingScripts/statusTracking.py
