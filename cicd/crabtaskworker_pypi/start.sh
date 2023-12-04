44470f3c4193aad148333986017f4e8cd8180430
Thanayut Seethongchuen <=>
2023-12-04 20:31:57 +0100

    typo

--- cicd/crabtaskworker_pypi/start.sh
+++ cicd/crabtaskworker_pypi/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # TASKWORKER_HOME only use in this script
 TASKWORKER_HOME=/data/srv/TaskManager
-CONFIG=$TASKWORKER_HOME/cfg/TaskWorkerConfig.p
+CONFIG=$TASKWORKER_HOME/cfg/TaskWorkerConfig.py
 # CRABTASKWORKER_ROOT is used to get where data directory is in `DagmanCreator.getLocation()`
 export CRABTASKWORKER_ROOT=/data/srv/current/lib/python3.8/site-packages/
 # export PYTHONPATH
