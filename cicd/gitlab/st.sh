728b88562c5ba473878c84ea6b263075092aeb45
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-02-06 11:09:55 +0100

    skip option and fix st.sh

--- cicd/gitlab/st.sh
+++ cicd/gitlab/st.sh
@@ -7,15 +7,21 @@ export CMSSW_release=CMSSW_13_0_2
 export Check_Publication_Status=Yes
 export CRABClient_version=prod
 cp artifacts/submitted_tasks_TS artifacts/submitted_tasks
-for i in {1..$RETRY}; do
-    echo "$i attempt."
+RETRY_COUNT=1
+while true; do
+    echo "$RETRY_COUNT/$RETRY attempt."
     bash -x cicd/gitlab/check_test_result.sh || rc=$?
     if [[ $rc -ne 0 ]]; then
         echo "check_test_result.sh is fail with exit code $rc"
         echo "sleep for $SLEEP_SECONDS seconds"
         sleep $SLEEP_SECONDS
+        if [[ $RETRY_COUNT -eq $RETRY ]]; then
+            echo "Reach max retry count: $RETRY"
+            exit 1
+        fi
+        RETRY_COUNT=$((RETRY_COUNT + 1))
         continue
     else
-        exit 0
+        break
     fi
 done
