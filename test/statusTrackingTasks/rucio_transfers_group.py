de953ad29ba3ee61f39ea1a3b7309f53c95f2a09
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-02-08 14:54:13 +0100

    fix group async dest (already fix in master)

--- test/statusTrackingTasks/rucio_transfers_group.py
+++ test/statusTrackingTasks/rucio_transfers_group.py
@@ -42,7 +42,7 @@ config.section_("User")
 config.section_("Site")
 config.Site.whitelist = ['T1_*','T2_US_*','T2_IT_*','T2_DE_*','T2_ES_*','T2_FR_*','T2_UK_*']
 #config.Site.blacklist = ['T2_ES_IFCA']
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T2_IT_Legnaro'
 
 config.section_("Debug")
 config.Debug.scheddName = 'crab3@vocms059.cern.ch'
