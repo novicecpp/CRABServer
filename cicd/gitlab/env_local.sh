c6591cb9795a22fe526e4d7152bc3e3bf3534c48
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-03-12 14:45:09 +0100

    env_local.sh for test locally

---
+++ cicd/gitlab/env_local.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+export CMSSW_release=CMSSW_13_0_2
+#fixed vars
+export CRABClient_version=prod
+export CRABServer_tag=HEAD
+export REST_Instance=test12
+export Client_Configuration_Validation=true
+#it should not need in future
+export Repo_GH_Issue=novicecpp/CRABServer
+export Repo_Testing_Scripts=novicecpp/CRABServer
+export Branch_Testing_Scripts=wmcore_pypi
+export Test_Docker_Image=registry.cern.ch/cmscrab/crabtesting:231009
+#bash -x cicd/gitlab/execute_test.sh
