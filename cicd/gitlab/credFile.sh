054e7b742819d13c8ba3dbf17e36063e9f177092
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-04-24 18:14:51 +0200

    allow to fall back proxy to installed cert in runner machine

--- cicd/gitlab/credFile.sh
+++ cicd/gitlab/credFile.sh
@@ -2,9 +2,28 @@
 set -euo pipefail
 
 CREDS_FILE=$1
+CREDS_TYPE=$2
+
+case "${CREDS_TYPE}" in
+    x509)
+        >&2 openssl x509 -checkend 0 -noout -in "${CRED_FILE}"
+        if [[ $? -ne 0 ]]; then
+            >&2 echo "Proxy file has expired. Generating new one from local cert..."
+            voms-proxy-init --rfc --voms cms -valid 196:00
+            CREDS_FILE="$(voms-proxy-info -path)"
+        fi
+        ;;
+    ssh)
+        : # noop
+        ;;
+    *)
+      >&2 echo "ERROR: Unknown CREDS_TYPE: ${CREDS_TYPE}"
+      exit 1
+      ;;
+esac
+
 CREDS_COPIED=$(basename ${CREDS_FILE})_copy
 cp ${CREDS_FILE} ${CREDS_COPIED}
-#echo >> ${CRAB_TW_SSH_KEY_COPIED}
 chown $(id -u):$(id -g) "${CREDS_COPIED}"
 chmod 0600 "${CREDS_COPIED}"
 realpath "${CREDS_COPIED}"
