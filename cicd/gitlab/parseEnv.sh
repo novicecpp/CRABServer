eeb8f9219e3f1809b5a5be8f0315408dd3b8e534
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-05-21 14:15:04 +0200

    add new env and use test12 instead of devthree

--- cicd/gitlab/parseEnv.sh
+++ cicd/gitlab/parseEnv.sh
@@ -2,8 +2,11 @@
 
 # Parse deployment env from tag.
 # - If match regexp `^pypi-(<env1>|<env2>|...)-.*`, set ENV_NAME to the string
-#     in group.
+#     in group. Valide env are preprod/test2/test11/test2
 # - If match release tag (e.g., v3.240501), set ENV_NAME to preprod
+#   - If tag have double underscore, the RELEASE_NAME (for final image tag) is
+#       the left side.
+#
 # Allow override ENV_NAME (from push option or WebUI)
 # dot-env files are in ./env directory.
 
@@ -11,10 +14,9 @@ set -euo pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 TAG=$1
-VALIDATE_DEV_TAG='^pypi-(devthree|preprod)-.*'
+VALIDATE_DEV_TAG='^pypi-(preprod|test2|test11|test12)-.*'
 VALIDATE_RELEASE_TAG='^v3\.[0-9]{6}.*'
-# Do not quote regexp vars here
-if [[ $TAG =~ $VALIDATE_DEV_TAG ]]; then
+if [[ $TAG =~ $VALIDATE_DEV_TAG ]]; then # Do not quote regexp variable here
     IFS='-' read -ra TMPSTR <<< "${TAG}"
     ENV_NAME=${ENV_NAME:-${TMPSTR[1]}}
 elif [[ $TAG =~ $VALIDATE_RELEASE_TAG ]]; then
@@ -24,6 +26,8 @@ else
     exit 1
 fi
 
+# Get the left side of tag
+# e.g., v3.220220.test1__1716292861, the release name will be v3.220220.test1
 VALIDATE_SPLITTER="^.+__.+"
 if [[ $TAG =~ $VALIDATE_SPLITTER ]]; then
     IFS='__' read -ra TMPARRAY <<< "${TAG}"
