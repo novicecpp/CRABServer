5fa6382841925ce4a45716759190d3474ca180b6
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-06-26 14:28:25 +0200

    comments again

--- src/python/TaskWorker/ChildWorker.py
+++ src/python/TaskWorker/ChildWorker.py
@@ -36,16 +36,9 @@ def startChildWorker(config, work, workArgs, logger):
     """
     procTimeout = config.FeatureFlags.childWorkerTimeout
     # we cannot passing the logger object to child worker.
-    loggerConfig = {
-        'name': logger.name,
-        # hardcode formatter to make in more simple. The format is copied from:
-        # https://github.com/dmwm/CRABServer/blob/43a8454abec4059ae5b2804b4efe8e77553d1f38/src/python/TaskWorker/Worker.py#L30
-        'format': "%(asctime)s:%(levelname)s:%(module)s %(message)s",
-        # also hardcode the log level
-        'level': logging.DEBUG,
-    }
+    loggerName = logger.name,
     with ProcessPoolExecutor(max_workers=1, mp_context=mp.get_context('fork')) as executor:
-        future = executor.submit(_runChildWorker, work, workArgs, procTimeout, loggerConfig)
+        future = executor.submit(_runChildWorker, work, workArgs, procTimeout, loggerName)
         try:
             outputs = future.result(timeout=procTimeout+1)
         except BrokenProcessPool as e:
@@ -64,11 +57,11 @@ def _signalHandler(signum, frame):
     """
     raise TimeoutError("The process reached timeout.")
 
-def _runChildWorker(work, workArgs, timeout, loggerConfig):
+def _runChildWorker(work, workArgs, timeout, loggerName):
     """
     The wrapper function to start running `work()` on the child-worker.
 
-    Note about loggerName argument, logging object cannot be pickled so we pass
+    Note about loggerConfig argument, logging object cannot be pickled so we pass
     the logging configuration and set it up on child-worker side.
 
     :param work: a function that need to run in child process
@@ -83,14 +76,25 @@ def _runChildWorker(work, workArgs, timeout, loggerConfig):
     :returns: return value from `work()`
     :rtype: any
     """
-    logger = logging.getLogger(f'{loggerConfig["name"]}.ChildWorker')
+    procName = f'{loggerName}.ChildWorker'
+    logger = logging.getLogger(procName)
+    # not 100% sure what is going on here but StreamHandler make the logs
+    # from child process go through parent process and write out to
+    # process/tasks log files.
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(loggerConfig['format']))
+    # hardcode formatter to make in more simple. The format is based on:
+    # https://github.com/dmwm/CRABServer/blob/43a8454abec4059ae5b2804b4efe8e77553d1f38/src/python/TaskWorker/Worker.py#L30
+    formatter = f"%(asctime)s:%(levelname)s:%(module)s:{procName}: %(message)s"
+    handler.setFormatter(logging.Formatter(formatter))
+    # also hardcode the log level
+    handler.setLevel(logging.DEBUG)
     logger.addHandler(handler)
-    logger.setLevel(loggerConfig['level'])
-    logger.info(f'Installing SIGALARM with timeout {timeout} seconds.')
+
+    # main
+    logger.debug(f'Installing SIGALARM with timeout {timeout} seconds.')
     signal.signal(signal.SIGALRM, _signalHandler)
     signal.alarm(timeout)
     outputs = work(*workArgs)
+    logger.debug('Uninstalling SIGALARM.')
     signal.alarm(0)
     return outputs
