276d489490b35020e303814997433373561ef0cf
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-06-26 10:57:29 +0200

    done test ChildWorker

--- test/python/TaskWorker/test_ChildWorker.py
+++ test/python/TaskWorker/test_ChildWorker.py
@@ -1,6 +1,8 @@
-from argparse import Namespace
 import time
 import pytest
+import ctypes
+from argparse import Namespace
+from unittest.mock import Mock
 
 from WMCore.Configuration import ConfigurationEx
 from TaskWorker.ChildWorker import startChildWorker
@@ -15,13 +17,43 @@ def config_ChildWorker():
     config.TaskWorker.childWorkerTimeout = 1
     return config
 
-def fn(n, timeSleep=1):
-    print(f'executing function with n={n},timeSleep={timeSleep}')
-    time.sleep(timeSleep)
-    print(f'return {n}*5')
+@pytest.fixture
+def mock_logger():
+    logger = Mock()
+    logger.name = '1'
+    return logger
+
+def fn(n, timeSleep=0, mode='any'):
+    print(f'executing function with n={n},timeSleep={timeSleep},mode={mode}')
+    if mode == 'timeout':
+        time.sleep(timeSleep)
+    elif mode == 'exception':
+        raise TypeError('simulate raise generic exception')
+    elif mode == 'coredump':
+        #https://codegolf.stackexchange.com/a/22383
+        ctypes.string_at(1)
+    else:
+        pass
     return n*5
 
-def test_startChildWorker_normal():
-    pass
-def test_startChildWorker_exception():
-    pass
+
+def test_startChildWorker_normal(config_ChildWorker, mock_logger):
+    n = 17
+    timeSleep = 0
+    mode = 'any'
+    startChildWorker(config_ChildWorker, fn, (n, timeSleep, mode), mock_logger)
+
+
+testList = [
+    (17, 0, 'any', None),
+    (17, 0, 'exception', TypeError),
+    (17, 5, 'timeout', ChildTimeoutException),
+    (17, 0, 'coredump', ChildUnexpectedExitException),
+]
+@pytest.mark.parametrize("n, timeSleep, mode, exceptionObj", testList)
+def test_executeTapeRecallPolicy_allow(n, timeSleep, mode, exceptionObj, config_ChildWorker, mock_logger):
+    if not exceptionObj:
+        startChildWorker(config_ChildWorker, fn, (n, timeSleep, mode), mock_logger)
+    else:
+        with pytest.raises(exceptionObj):
+            startChildWorker(config_ChildWorker, fn, (n, timeSleep, mode), mock_logger)
