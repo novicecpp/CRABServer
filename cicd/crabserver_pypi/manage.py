69e79153d6bfa1953dea564729b464a8cc6852dc
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-08-27 19:34:38 +0200

    comment2

--- cicd/crabserver_pypi/manage.py
+++ cicd/crabserver_pypi/manage.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 """
-The python helper to parse args
+The python helper to parse args (use pure bash is a nightmare).
+We can even convert shell script in to python to increase code maintainability.
 """
 import argparse
 import os
@@ -24,18 +25,31 @@ class EnvDefault(argparse.Action):
 
 parser = argparse.ArgumentParser(description='crab service process controller')
 
-subparsers = parser.add_subparsers(dest='command', required=True)
-parserStart = subparsers.add_parser('start')
+subparsers = parser.add_subparsers(dest='command', required=True, help='command to run')
+parserStart = subparsers.add_parser('start',
+                                    help='start the service')
 groupModeStart = parserStart.add_mutually_exclusive_group(required=True)
-groupModeStart.add_argument('-c', dest='mode', action='store_const', const='current')
-groupModeStart.add_argument('-g', dest='mode', action='store_const', const='fromGH')
-parserStart.add_argument('-d', dest='debug', action='store_const', const='t', default='f')
-parserStart.add_argument('-s', dest='service', action=EnvDefault, envvar='SERVICE')
-parserStop = subparsers.add_parser('stop')
-parserEnv = subparsers.add_parser('env')
+groupModeStart.add_argument('-c', dest='mode', action='store_const', const='current',
+                            help='start service from installed directory')
+groupModeStart.add_argument('-g', dest='mode', action='store_const', const='fromGH',
+                            help='start service from /data/repos directory')
+parserStart.add_argument('-d', dest='debug', action='store_const', const='t',
+                         default='f',
+                         help='Enable debug mode (foreground)')
+# env $SERVICE
+parserStart.add_argument('-s', dest='service', action=EnvDefault, envvar='SERVICE',
+                         help='Name of the service to run: REST/TaskWorker*/Publisher*')
+parserStop = subparsers.add_parser('stop',
+                                    help='show service status (exit non-zero if service does not start)')
+parserStatus = subparsers.add_parser('status',
+                                    help='start the service')
+parserEnv = subparsers.add_parser('env',
+                                  help='print environment variable for sourcing it locally.')
 groupModeEnv = parserEnv.add_mutually_exclusive_group(required=True)
-groupModeEnv.add_argument('-c', dest='mode', action='store_const', const='current')
-groupModeEnv.add_argument('-g', dest='mode', action='store_const', const='fromGH')
+groupModeEnv.add_argument('-c', dest='mode', action='store_const', const='current',
+                            help='export PYTHONPATH from installed directory')
+groupModeEnv.add_argument('-g', dest='mode', action='store_const', const='fromGH',
+                            help='export PYTHONPATH from /data/repos directory')
 
 args = parser.parse_args()
 
@@ -47,4 +61,5 @@ env['DEBUG'] = getattr(args, 'debug', '')
 env['SERVICE'] = getattr(args, 'service', '')
 
 # re exec the ./manage.sh
+# os.execle(filepath, arg0, arg1, ..., argN, env_dict)
 os.execle('./manage.sh','./manage.sh', env)
