799ba6626f8a4803cb66a2d4061ff3bc27a91e42
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-09-03 16:52:38 +0200

    fix script and dockerfile

--- cicd/crabserver_pypi/installWMCore.sh
+++ cicd/crabserver_pypi/installWMCore.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-reqfile=${1}
+if [[ $# -ne 2 ]]; then
+    >&2 echo "Error: position arguments required."
+    >&2 echo "Usage: $0 <wmcore_requirements.txt_path> <install_path>"
+    exit 1
+fi
+
+reqfile="${1}"
+installpath="${2}"
+
+if [[ ! -d "${installpath}" ]]; then
+    >&2 echo "Error: Install path ${installpath} does not exist or not directory."
+    exit 1
+fi
+
 wmcore_repo="$(grep -v '^\s*#' "${reqfile}" | cut -d' ' -f1)"
 wmcore_version="$(grep -v '^\s*#' "${reqfile}" | cut -d' ' -f2)"
 if [[ ${wmcore_repo} =~ /github.com\/dmwm\/WMCore ]]; then
@@ -11,5 +24,6 @@ else
     # simply copy all src/python to install directory
     echo "Installing WMCore ${wmcore_version} from ${wmcore_repo} via clone..."
     git clone  --depth 1 "${wmcore_repo}" -b "${wmcore_version}" WMCore
-    cp -rp WMCore/src/python/* /data/srv/current/lib/python/site-packages/
+    cp -rp WMCore/src/python/* "${installpath}"
+    rm -rf WMCore
 fi
